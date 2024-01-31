4edb6b798bb988a4d7bba6892d48b30f8f156b49
akki-thinktanker <akki.thinktanker@gmail.com>
2024-01-31 16:33:39 +0530

    video 6 done

---
+++ Chai aur backend/Video 6/src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: './env'})
+
+// This is an old approach
+
+// new and usable approach
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+///////////////////////////////////////////////
+// 1st approach
+
+/*
+import express from 'express'
+const app = express()
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on('error', (error) => {
+        console.log("Error:", error)
+        throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+        console.log(`App is listening on PORT ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.log("Error:", error);
+    throw error;
+  }
+})();
+*/
