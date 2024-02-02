5c8278f420718efd8cc1138cf7b4033de1b2d298
akki-thinktanker <akki.thinktanker@gmail.com>
2024-02-02 10:07:21 +0530

    forgot to add round brackets after cookieParser, fixed by making it cookieParser() for middleware

--- Chai aur backend/Video 6/src/app.js
+++ Chai aur backend/Video 6/src/app.js
@@ -14,14 +14,13 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-app.use(cookieParser);
+app.use(cookieParser()); // error: here I used cookieParser without brackets
 
 // routes import
 import userRouter from "./routes/user.routes.js";
 
 // routes declaration
-app.use("/users", userRouter);
-// app.use("/api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 // http://localhost:8000/api/v1/users/register
 
 export { app };
--- Chai aur backend/Video 6/src/routes/user.routes.js
+++ Chai aur backend/Video 6/src/routes/user.routes.js
@@ -3,7 +3,6 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.route("/register").get(registerUser);
-// router.route("/login").get(loginUser);
+router.route("/register").post(registerUser);
 
 export default router;
