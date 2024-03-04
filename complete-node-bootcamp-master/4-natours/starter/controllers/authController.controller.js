3b104727cdae606c4fa5a93bb716492d33a6c7c5
akki-thinktanker <akki.thinktanker@gmail.com>
2024-03-04 10:12:06 +0530

    auth controller changes

--- complete-node-bootcamp-master/4-natours/starter/controllers/authController.controller.js
+++ complete-node-bootcamp-master/4-natours/starter/controllers/authController.controller.js
@@ -2,7 +2,7 @@ const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel.model");
 const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
+const sendEmail = require("../utils/email");
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -117,8 +117,35 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 2) Generate the random token
   const resetToken = user.createPasswordResetToken();
 
-  await user.save({validateBeforeSave: false});
+  await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
+
+  const resetURL = `${req.protocol}://${req.get("host")}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit your patch request with your new password and passwordConfirm to :${resetURL}.\n If you didn't forget your password, Please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Your password reset token (valid for 10 minutes)",
+      message,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "Token sent to email!",
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError("There was an error sending the email. Try again later!"),
+      500
+    );
+  }
 });
 exports.resetPassword = (req, res, next) => {};
